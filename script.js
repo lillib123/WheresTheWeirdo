f3ff07886edb3179d54a1f00f6c67152bf336f49
Lillianna Blair <lblair@lyonscg.com>
2018-11-28 13:33:41 -0500

    added parameter option for variable number of divs

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
 let wins = 0;
-$('.win-container').text('Wins: ' + wins);
+
+setupBlocks(1000);
 
 $('#guy_container').on('click', function() {
   move_guy();
 })
 
-function move_guy() {
+// function definitions
+function setupBlocks(numberOfBlocks) {
+  for (let i=1; i<=numberOfBlocks; i++) {
+    $('body').append('<div class = "d"></div>');
+  }
+}
 
+function move_guy() {
   counter();
-  var left_margin = Math.floor((Math.random() * 1000) + 1);
+  let left_margin = Math.floor((Math.random() * 1000) + 1);
   $('#guy').css('margin-left', left_margin + 'px');
   $('#guy_container').css('margin-left', left_margin + 'px');
 
-  var top_margin = Math.floor((Math.random() * 1000) + 1);
+  let top_margin = Math.floor((Math.random() * 1000) + 1);
   $('#guy').css('margin-top', top_margin + 'px');
   $('#guy_container').css('margin-top', top_margin + 'px');
 }
 
-function counter() {  $('.win-counter').text('Wins: ' + wins);
-  if (wins === 0){
-    $('#guy').css('width', '150px');
-    wins = wins + 1;
-  } else if (wins === 1) {
-    $('#guy').css('width', '120px');
-    wins = wins + 1;
-  } else if (wins === 2) {
-    $('#guy').css('width', '90px');
-    wins = wins + 1;
-  } else if (wins === 3) {
-    $('#guy').css('width', '60px');
-    wins = wins + 1;
-  }else if (wins === 4) {
-    $('#guy').css('width', '30px');
-    wins = wins + 1;
-  } else return;
+function counter() {
+  if (wins < 10) {
+    wins++;
+    $('.win-container').text('Wins: ' + wins);
+  } else {
+    $('.win-container').text('YOU WIN!');
+  }
 }
